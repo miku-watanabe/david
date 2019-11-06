e3111969f1420d62e91ce6eda7092ae200f70c80
miku-watanabe <m.watanabe@dcworks.jp>
2019-11-06 18:26:01 +0900

    cccwq

--- wtnb-test.php
+++ wtnb-test.php
@@ -1,8 +1,28 @@
-public function index()
-{
-    $a = 1280;
-    // $b = 2500;
-    $b = '2500';
+<?php
+declare(strict_types=1);
 
-    return $this->addition($a, $b);
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function index()
+    {
+        $a = 1280;
+        $b = 2500;
+
+        return $this->addition($a, $b);
+    }
+
+    /**
+     * 加算
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
+    private function addition(int $a, int $b): int
+    {
+        return $a + $b;
+    }
 }
