311016d16f2d1d14c8fd519d9c11552024bd9264
miku-watanabe <m.watanabe@dcworks.jp>
2019-11-06 18:23:22 +0900

    bbb

--- wtnb-test.php
+++ wtnb-test.php
@@ -1,12 +1,8 @@
-<!php
 public function index()
-    {
-        $a = 1280;
-        $b = 2500;
-
-        return $this->addition($a, $b);
-    }
-
+{
     $a = 1280;
-    $b = 2500;
+    // $b = 2500;
+    $b = '2500';
 
+    return $this->addition($a, $b);
+}
